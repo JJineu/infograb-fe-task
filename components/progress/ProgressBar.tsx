ce21aee3b3cf1af08492b019e7ac4f24fb98c10e
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2024-07-01 00:40:08 +0900

    :bulb: [ Update ] component - ProgressBar ui 업데이트

--- components/progress/ProgressBar.tsx
+++ components/progress/ProgressBar.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const ProgressBar = ({ progress, step, onClick }: { progress: number; step: string[]; onClick: (index: number) => void }) => {
+const ProgressBar = ({ progress, steps, onClick }: { progress: number; steps: string[]; onClick: (index: number) => void }) => {
+  const calculateProgress = (index: number, stepsLength: number) => progress > (index / stepsLength) * 100;
+
   return (
-    <div className='relative flex h-10 justify-evenly'>
-      <div className='absolute left-0 top-4 h-2 bg-blue-600' style={{ width: `${progress}%` }} />
-      {step.map((_, index) => (
+    <div className='relative flex h-10 justify-between'>
+      <div className='absolute left-0 top-4 h-2 bg-blue-500 transition-all duration-200 ease-linear' style={{ width: `${progress}%` }} />
+      {steps.map((step, index) => (
         <div
-          key={index}
-          className='z-10 size-10 rounded-full bg-blue-400 text-center'
-          onClick={() => {
-            onClick(index);
-          }}
+          key={step}
+          className={`z-10 -mr-2 mt-[7px] size-7 rounded-full border-2 text-center text-white ${calculateProgress(index + 1, steps.length) ? 'border-transparent bg-green-500' : 'border-slate-300 bg-white'}`}
+          onClick={() => calculateProgress(index + 1, steps.length) && onClick(index)}
         >
-          {index + 1}
+          {calculateProgress(index + 1, steps.length) ? '✓' : ''}
         </div>
       ))}
     </div>
