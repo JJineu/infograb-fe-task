d61181e552c0741ca4905ded364b1e0d37091d45
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2024-06-30 23:45:48 +0900

    :bulb: [ Update ] Loading - style 업데이트

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className='fixed left-1/2 top-1/2 z-[100] flex size-full -translate-x-1/2 -translate-y-1/2 items-center justify-center bg-slate-50 opacity-75 md:ml-20 dark:bg-zink-700'>
+      <div className='loader-loading mb-36 h-[38px] w-[200px] overflow-hidden ' />
+    </div>
+  );
 };
 
 export default Loading;
