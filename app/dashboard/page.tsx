89d124b7c2770137c435d2ddede376c45472502c
hyunjin <joice.kim7@gmail.com>
2024-07-01 15:10:50 +0900

    :bulb: [ Update ] pages text 내용 추가

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,55 @@
+'use client';
+
 import React from 'react';
 
+import BarChart from '@/components/chart/BarChart';
+import BoxPlotChart from '@/components/chart/BoxPlotChart';
+import { getStandardDeviationResult, getSumAvgResult, getSumResult } from '@/data/services';
+
 const DashboardPage = () => {
+  const sumData = getSumResult();
+  const sumAvgData = getSumAvgResult();
+  const standardDeviationData = getStandardDeviationResult();
+
   return (
-    <div>
-      <section>
-        <article>팀별 합계 차트</article>
-        <article>팀별 평균 차트</article>
-        <article>팀별 총합의 표준편차 차트</article>
-      </section>
-      <section>
-        <article>기타 통계</article>
-        <article>기타 통계</article>
-        <article>기타 통계</article>
+    <div className='p-4'>
+      <section className='flex flex-wrap gap-2'>
+        <article>
+          <h5>팀별 합계 차트</h5>
+          <div className='size-[500px]'>
+            <BarChart data={sumData} />
+          </div>
+        </article>
+        <article>
+          <h5>팀별 평균 차트</h5>
+          <div className='size-[500px]'>
+            <BarChart data={sumAvgData} />
+          </div>
+        </article>
+        <article>
+          <h5>팀별 총합의 표준편차 차트</h5>
+          <div className='size-[500px]'>
+            <BoxPlotChart data={standardDeviationData} />
+          </div>
+        </article>
+        {/* <article>
+          <h5></h5>
+          <div className='size-[500px]'>
+            <BarChart data={sumData} />
+          </div>
+        </article>
+        <article>
+          <h5></h5>
+          <div className='size-[500px]'>
+            <BarChart data={sumData} />
+          </div>
+        </article>
+        <article>
+          <h5></h5>
+          <div className='size-[500px]'>
+            <BarChart data={sumData} />
+          </div>
+        </article> */}
       </section>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { UserForm } from '@/components/user';
 
 const HomePage = () => {
   return (
-    <section className='mt-10 flex flex-col items-center gap-10 p-1'>
-      <h2>설문제목</h2>
-      <p>설명</p>
+    <section className='mt-10 flex flex-col items-center gap-10 p-4'>
+      <h2>회사 근처 맛집 설문조사</h2>
+      <p>회사 근처에는 어떤 맛집들이 있을까요? 이 설문조사에 참여하여 회사 주변 맛집에 대한 솔직한 의견을 들려주세요!</p>
       <UserForm />
     </section>
   );
